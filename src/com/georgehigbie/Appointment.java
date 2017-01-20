a833ffe4889fe4c769d4d6a69567315dfa2a6fcb
George Higbie <gehigbie@gmail.com>
2017-01-19 17:50:24 -0700

    added apointment file

---
+++ src/com/georgehigbie/Appointment.java
@@ -0,0 +1,11 @@
+package com.georgehigbie;
+
+/**
+ * Created by georgehigbie on 1/19/17.
+ */
+public class Appointment {
+
+    String patient;
+    String doctor;
+    g
+}
---
+++ src/com/georgehigbie/Calendar.java
@@ -0,0 +1,9 @@
+package com.georgehigbie;
+
+/**
+ * Created by georgehigbie on 1/19/17.
+ */
+public class Calendar {
+
+
+}
